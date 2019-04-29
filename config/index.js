f74c9536d43e98ec1dc0bad7ede9beeff0d881b7
LorenzoVernazza <lorenzo.vernazza@circlegarage.com>
2019-04-29 14:39:18 +0200

    first commit

---
+++ config/index.js
@@ -0,0 +1,42 @@
+exports.levels = [{
+	level: 'fatal',
+	color: 'bgRed',
+	error: true
+}, {
+	level: 'error',
+	color: 'red',
+	error: true
+}, {
+	level: 'warn',
+	color: 'yellow'
+}, {
+	level: 'success',
+	color: 'green'
+}, {
+	level: 'info',
+	color: 'cyan'
+}, {
+	level: 'debug',
+	color: 'magenta'
+}, {
+	level: 'trace',
+	color: 'blue'
+}];
+
+exports.defaults = {
+	name: '',
+	nameColor: 'grey',
+	nameTransform: 'uppercase',
+
+	dateFormat: 'D MMM YYYY, HH:mm:ss.SSS Z',
+
+	separator: '-',
+
+	colorObjects: 'true',
+	inspectDepth: 3,
+	level: 'debug',
+
+	showDate: 'true',
+	showLevel: 'true',
+	showName: 'true'
+};
